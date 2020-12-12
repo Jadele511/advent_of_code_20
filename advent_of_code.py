4f55547a852f6ea8563ee0237c607bb903871c87
Jade Le <baongocle1991@gmail.com>
2020-12-12 10:25:23 -0800

    aoc d2

--- advent_of_code.py
+++ advent_of_code.py
@@ -32,3 +32,38 @@ def sum_three_2020(file1):
 file2 = open("aoc_d2.txt")
 
 
+def valid_password(file2):
+
+    valid_pw_count = 0
+
+    for line in file2:
+        least_to_most, letter, pw = line.split(' ')
+
+        least = int(least_to_most.split('-')[0])
+        most = int(least_to_most.split('-')[1])
+        letter = letter[0]
+
+        letter_count = pw.count(letter)
+        if letter_count >= least and letter_count <= most:
+            valid_pw_count += 1
+    return valid_pw_count
+
+
+
+
+def valid_password_2(file2):
+
+    valid_pw_count = 0
+
+    for line in file2:
+        indexes, letter, pw = line.split(' ')
+
+        first_idx = int(indexes.split('-')[0])
+        second_idx = int(indexes.split('-')[1])
+        letter = letter[0]
+
+        if (pw[first_idx-1] == letter and pw[second_idx-1] != letter) or (pw[first_idx-1] != letter and pw[second_idx-1] == letter):
+            valid_pw_count += 1 
+
+    return valid_pw_count
+
